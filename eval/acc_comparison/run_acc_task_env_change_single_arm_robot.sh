2975bfdf6536f58cb3db29a2ad9f13e4cef15437
LINC-BIT <379068433@qq.com>
2026-08-31 08:07:23 +0000

    update

--- eval/acc_comparison/run_acc_task_env_change_single_arm_robot.sh
+++ eval/acc_comparison/run_acc_task_env_change_single_arm_robot.sh
@@ -16,7 +16,75 @@ fi
 
 ACC_SINGLE_WORKLOAD_SERIAL_GPU="${ACC_SINGLE_WORKLOAD_SERIAL_GPU:-0}"
 if [[ -z "${GPU_BY_METHOD_OVERRIDE:-}" ]]; then
-    GPU_BY_METHOD_OVERRIDE="conrft=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},flare=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},improv_vla=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},edgeta=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},convertnet=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},ours_single_agent=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},ppo_gen=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},self_improv=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},vla_rft=${ACC_SINGLE_WORKLOAD_SERIAL_GPU},world_env=${ACC_SINGLE_WORKLOAD_SERIAL_GPU}"
+    build_serial_gpu_override() {
+        local family="$1"
+        local raw_methods="${METHODS:-${RUN_METHODS:-}}"
+        local -a resolved_methods=()
+        local method=""
+
+        if [[ -n "$raw_methods" ]]; then
+            while IFS= read -r method; do
+                method="${method#${method%%[![:space:]]*}}"
+                method="${method%${method##*[![:space:]]}}"
+                [[ -z "$method" ]] && continue
+                case "$method" in
+                    vlaselect)
+                        if [[ "$family" == "octo" ]]; then
+                            resolved_methods+=("ours_single_agent")
+                        else
+                            resolved_methods+=("ours")
+                        fi
+                        ;;
+                    ours)
+                        if [[ "$family" == "octo" ]]; then
+                            resolved_methods+=("ours_single_agent")
+                        else
+                            resolved_methods+=("ours")
+                        fi
+                        ;;
+                    ours_single_agent)
+                        if [[ "$family" == "octo" ]]; then
+                            resolved_methods+=("ours_single_agent")
+                        else
+                            resolved_methods+=("ours")
+                        fi
+                        ;;
+                    *)
+                        resolved_methods+=("$method")
+                        ;;
+                esac
+            done < <(printf '%s' "$raw_methods" | tr ',' '
+')
+        fi
+
+        if [[ "${#resolved_methods[@]}" -eq 0 ]]; then
+            case "$family" in
+                octo)
+                    resolved_methods=(conrft flare improv_vla edgeta convertnet ours_single_agent ppo_gen self_improv vla_rft world_env)
+                    ;;
+                edgevla|tinyvla)
+                    resolved_methods=(ppo_gen ours conrft flare edgeta convertnet improv_vla self_improv vla_rft world_env)
+                    ;;
+                vla_adapter_new)
+                    resolved_methods=(conrft flare improv_vla edgeta convertnet ours ppo_gen self_improv vla_rft world_env)
+                    ;;
+                *)
+                    resolved_methods=(ours)
+                    ;;
+            esac
+        fi
+
+        local out=""
+        for method in "${resolved_methods[@]}"; do
+            if [[ -n "$out" ]]; then
+                out+="," 
+            fi
+            out+="${method}=${ACC_SINGLE_WORKLOAD_SERIAL_GPU}"
+        done
+        printf '%s' "$out"
+    }
+
+    GPU_BY_METHOD_OVERRIDE="$(build_serial_gpu_override "$MODEL_SELECTION")"
 fi
 
 exec env MODEL_SELECTION="$MODEL_SELECTION" ENV_ID_ORDER="$ENV_ID_ORDER" GPU_BY_METHOD_OVERRIDE="$GPU_BY_METHOD_OVERRIDE" bash "$SCRIPT_DIR/run_acc_task_env_change.sh" "$@"
